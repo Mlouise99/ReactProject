bb4d277cc5ac59ad259fd7c1febe5aab7ff2463c
Mlouise99 <mwubahalouise99@gmail.com>
2024-03-25 21:50:15 +0200

    try to understand how react works

---
+++ react-project/src/App.jsx
@@ -0,0 +1,28 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  return (
+    <>
+  <div className='someValue'>
+    
+      <h2> My First React APP</h2>
+      <ul>
+        <li>
+          <a href="#">hello world</a>
+        </li>
+      </ul>
+      </div>
+      <h2> How are you doing?</h2> 
+      <input type="text" name='' id='' />
+</>
+
+  ); 
+
+
+} 
+
+
+export default App
