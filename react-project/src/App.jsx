10bc8ff9a3284cb7c8a4b365b0d21d0250265c33
Mlouise99 <mwubahalouise99@gmail.com>
2024-03-26 17:08:28 +0200

    getting series of react

--- react-project/src/App.jsx
+++ react-project/src/App.jsx
@@ -1,28 +1,16 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-
+import Youtube from "./assets/Youtube"
 function App() {
   return (
-    <>
-  <div className='someValue'>
-    
-      <h2> My First React APP</h2>
-      <ul>
-        <li>
-          <a href="#">hello world</a>
-        </li>
-      </ul>
-      </div>
-      <h2> How are you doing?</h2> 
-      <input type="text" name='' id='' />
-</>
+    <> 
+    <h1>Class react app</h1>
+    < youtube/>
+    </>    
+     
+   
+  )
 
-  ); 
+}
 
-
-} 
-
+ 
 
 export default App
--- react-project/src/main.jsx
+++ react-project/src/main.jsx
@@ -1,10 +1,23 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
-import './index.css'
+
+
+const reactElement = {
+  type: 'a',
+  props: {
+      href: "https://google.com",
+      target: '_blank'
+  },
+  children: 'Click here to visit google'
+};
+const AnotherElement = (
+  < a href='https://google.com' target='_blank'> Visit Google </a>
+) 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
-)
+  AnotherElement
+) 
